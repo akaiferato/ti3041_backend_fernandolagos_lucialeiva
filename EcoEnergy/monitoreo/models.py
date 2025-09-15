eef386b7bd23f62d1b4b181a93dd090eec797dec
akaiferato <fernandoalagosc@gmail.com>
2025-09-15 03:59:01 -0300

    se añaden modelos base para comenzar a trabajar con ramas los template, url y views

--- EcoEnergy/monitoreo/models.py
+++ EcoEnergy/monitoreo/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Zone(BaseModel):
+    device_zone = models.CharField(max_length=20)
+    coordinates = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.device_zone
+
+
+class Device(BaseModel):
+    name = models.CharField(max_length=20)
+    state = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(BaseModel):
+    intake = models.IntegerField()
+    date = models.DateField()
+    intake_limit = models.IntegerField()
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.intake)
+
+
+class Alert(BaseModel):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    measurement = models.OneToOneField(Measurement, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
