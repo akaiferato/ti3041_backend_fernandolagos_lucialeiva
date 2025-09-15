325652af4e9c14382b041c68e94ff2dba17412cf
akaiferato <fernandoalagosc@gmail.com>
2025-09-15 14:45:50 -0300

    se agregan borradores de template, modelos y views finales

--- EcoEnergy/monitoreo/models.py
+++ EcoEnergy/monitoreo/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         abstract = True
+
+
+class Organization(BaseModel):
+    name = models.CharField(max_length=20)
+    rut = models.CharField(max_length=15)
+    field = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(BaseModel):
@@ -29,8 +39,9 @@ class Zone(BaseModel):
 class Device(BaseModel):
     name = models.CharField(max_length=20)
     state = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='devices')
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE, related_name='devices')
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name='devices')
 
     def __str__(self):
         return self.name
@@ -38,18 +49,22 @@ class Device(BaseModel):
 
 class Measurement(BaseModel):
     intake = models.IntegerField()
-    date = models.DateField()
-    intake_limit = models.IntegerField()
-    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name="measurements")
 
     def __str__(self):
         return str(self.intake)
 
 
 class Alert(BaseModel):
-    name = models.CharField(max_length=20)
+    class Level(models.TextChoices):
+        GRAVE = 'GRV', 'Grave'
+        ALTA = 'ALT', 'Alta'
+        MEDIA = 'MED', 'Media'
+
+    level = models.CharField(max_length=3, choices=Level.choices)
     description = models.TextField()
-    measurement = models.OneToOneField(Measurement, on_delete=models.CASCADE)
+    measurement = models.OneToOneField(Measurement, on_delete=models.CASCADE, related_name='alerts')
 
     def __str__(self):
-        return self.name
+        return self.level
--- EcoEnergy/monitoreo/views.py
+++ EcoEnergy/monitoreo/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from datetime import timedelta
+from django.db.models import Count
 
-# Create your views here.
+from .models import Organization, Category, Zone, Device, Measurement, Alert
+
+
+def dashboard(request):
+    devices_categories = Category.objects.annotate(n_devices=Count('devices')).order_by('name')
+    
+    devices_zones = Zone.objects.annotate(n_devices=Count('devices')).order_by('devices_zone')
+
+    week_ago = timezone.now() - timedelta(days=7)
+    week_alerts = Alert.objects.filter(alerts__date__gte=week_ago)
+    alert_summary = {
+            'graves' : week_alerts.filter(level=Alert.Level.GRAVE).count(),
+            'altas' : week_alerts.filter(level=Alert.Level.ALTA).count(),
+            'medias' : week_alerts.filter(level=Alert.Level.MEDIA).count()
+            }
+    
+    last10_measures = Measurement.objects.order_by('-timestamp')[:10]
+
+    context = {
+            "devices_categories":devices_categories,
+            "devices_zones" : devices_zones,
+            "alert_summary" : alert_summary,
+            "last10_measures" : last10_measures
+            }
+
+    return render(request, "monitoreo/dashboard.html", context)
+
+def device_list(request):
+    devices = Device.objects.select_related('category', 'zone')
+    
+    category_id = request.GET.get('category')
+    if category_id:
+        devices = devices.filter(category__id=category_id)
+
+    categories = Category.objects.all()
+
+    context = {
+        'devices': devices,
+        'categories': categories,
+        'marked_category': int(category_id) if category_id else None
+    }
+    return render(request, 'monitoreo/lista_dispositivos.html', context)
+
+def device_details(request, device_id):
+    device = get_object_or_404(Device, pk=device_id)
+    
+    measurements = device.measurements.order_by('-timestamp')[:50]
+    alerts = device.measurements.alerts.order_by('-timestamp')
+
+    context = {
+        'device': device,
+        'measurements': measurements,
+        'alerts': alerts
+    }
+    return render(request, 'monitoreo/detalle_dispositivo.html', context)
+
+def measurements_list(request):
+    measurements = Measurement.objects.select_related('device').order_by('-timestamp')
+
+    context = {'measurements' : measurements}
+
+    return render(request, "monitoreo/lista_mediciones.html", context)
