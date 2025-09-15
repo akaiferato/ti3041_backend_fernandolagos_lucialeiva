d0b2cd8425bc67816d7c40ffac2f892bc1fd3033
akaiferato <fernandoalagosc@gmail.com>
2025-09-15 14:58:20 -0300

    admin basico operativo y retoque a settings (agregar aplicaciones al listado)

--- EcoEnergy/monitoreo/admin.py
+++ EcoEnergy/monitoreo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organization, Category, Zone, Device, Measurement, Alert
+
+admin.site.register([Organization, Category, Zone, Device, Measurement, Alert])
